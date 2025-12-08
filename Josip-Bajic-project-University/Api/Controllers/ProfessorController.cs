6987d6ee765554a97fbcdeadf417be1e3fab2911
Josip <jbajic00@fesb.hr>
2025-12-08 23:45:52 +0100

    HW02-renamed UniversityController in StudentController and changed code and added ProfessorController

---
+++ Josip-Bajic-project-University/Api/Controllers/ProfessorController.cs
@@ -0,0 +1,49 @@
+﻿using Application.DTOs;
+using Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProfessorController : ControllerBase
+    {
+        //private ProfessorRepository _professorRepository;
+
+        //public ProfessorController(ProfessorRepository professorRepository) 
+        //{
+        //    _professorRepository=professorRepository;
+        //}
+
+        [HttpGet]
+        public IEnumerable<Professor> GetAllProfessors()
+        {
+            return Enumerable.Empty<Professor>();
+        }
+
+        [HttpGet("{id}")]
+        public Professor GetProfessorById(int id)
+        {
+            return new Professor();
+        }
+
+        [HttpPost("new")]
+        public int AddProfessor([FromBody] PostProfessorDTO newProfessor)
+        {
+            return 0;
+        }
+
+        [HttpPut("edit/{id}")]
+        public int EditProfessor(int id, [FromBody] PostProfessorDTO professor)
+        {
+            return 0;
+        }
+
+        [HttpDelete("delete/{id}")]
+        public int DeleteProfessor([FromRoute] int id)
+        {
+            return 0;
+        }
+    }
+}
---
+++ Josip-Bajic-project-University/Api/Controllers/StudentController.cs
@@ -0,0 +1,50 @@
+﻿using Application.DTOs;
+using Domain.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        //private StudentRepository _studentRepository;
+       
+        //public StudentController(StudentRepository studentRepository) 
+        //{
+        //    _studentRepository=studentRepository;
+        //}
+
+        [HttpGet]
+        public IEnumerable<Student> GetAllStudents() 
+        {
+            return Enumerable.Empty<Student>();
+        }
+
+        [HttpGet("{id}")]
+        public Student GetStudentById(int id) 
+        {
+            return new Student();
+        }
+
+        [HttpPost("new")]
+        public int AddStudent([FromBody] PostStudentDTO newStudent)
+        {
+            return 0;
+        }
+
+        [HttpPut("edit/{id}")]
+        public int EditStudent(int id, [FromBody] PostStudentDTO student)
+        {
+            return 0;
+        }
+
+        [HttpDelete("delete/{id}")]
+        public int DeleteStudent([FromRoute] int id)
+        {
+            return 0; 
+        }
+
+    }
+}
