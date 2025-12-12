1288c86f61306c07a0ed199e3a3f4e73ffc0fb08
Josip <jbajic00@fesb.hr>
2025-12-12 16:21:36 +0100

    HW03-set db connection

---
+++ Josip-Bajic-project-University/Infrastructure/Persistance/UniversityDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Infrastructure.Persistance
+{
+    public class UniversityDbContext:DbContext, IApplicationDbContext
+    {
+        public UniversityDbContext(DbContextOptions<UniversityDbContext> options)
+            : base(options) { }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Professor> Professors { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(UniversityDbContext).Assembly);
+        }
+    }
+}
